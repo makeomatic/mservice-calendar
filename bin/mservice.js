8d1f3d6ad6af7284bfe4bf368720f502de75b43b
Dmitry Usa <atskiisotona@gmail.com>
2016-07-26 01:06:06 +0800

    Wait for crate before running

--- bin/mservice.js
+++ bin/mservice.js
@@ -5,8 +5,26 @@
 const configuration = require('ms-conf');
 const Service = require('../src');
 const service = new Service(configuration.get('/'));
+const Promise = require('bluebird');
+const url = require('url');
+
+const ping = Promise.promisify(require('./ping').ping);
 
 service.connect()
+    .then(function awaitCrate() {
+        const crateServer = url.parse(configuration.get('/').crate.connectionString);
+        return ping({
+            address: crateServer.hostname,
+            port: crateServer.port,
+            timeout: 10000,
+            attempts: 20
+        }).then(function parsePing(pingResult) {
+            const err = pingResult.results[pingResult.results.length - 1].err;
+            if (err) {
+                throw new Error(`Could not connect to Crate.io server ${crateServer.host}`);
+            }
+        })
+    })
     .then(function testPrepare() {
         if (process.env.NODE_ENV === 'test') {
             service.log.info('Started service, dropping tables');
