af0f8104185be6bd677968d5c0f37107c0d23351
Vitaly Aminev <AVVS@users.noreply.github.com>
2016-07-26 19:32:38 +0300

    feat: refactor code to be more inline with standard makeomatic style
    * refactor: add forced linting
    * tests: linting before tests
    * fix: bindings

--- bin/mservice.js
+++ bin/mservice.js
@@ -3,50 +3,59 @@
 // accepts conf through .env file
 // suitable for configuring this in the docker env
 const configuration = require('ms-conf');
-const Service = require('../src');
-const service = new Service(configuration.get('/'));
 const Promise = require('bluebird');
 const url = require('url');
-
 const ping = Promise.promisify(require('./ping').ping);
+const Service = require('../lib');
+
+const service = new Service(configuration.get('/'));
 
 function awaitCrate() {
-    const crateServer = url.parse(configuration.get('/').crate.connectionString);
-    return ping({
-        address: crateServer.hostname,
-        port: crateServer.port,
-        timeout: 5000,
-        attempts: 20
-    }).then(function parsePing(pingResult) {
-        if (!pingResult) {
-            throw new Error(`Could not connect to Crate.io server ${crateServer.host}`);
-        }
-    })
+  const crateServer = url.parse(configuration.get('/').crate.connectionString);
+  return ping({
+    address: crateServer.hostname,
+    port: crateServer.port,
+    timeout: 5000,
+    attempts: 20,
+  })
+  .then(function parsePing(pingResult) {
+    if (!pingResult) {
+      throw new Error(`Could not connect to Crate.io server ${crateServer.host}`);
+    }
+
+    return null;
+  });
 }
 
 function testPrepare() {
-    if (process.env.NODE_ENV === 'test') {
-        service.log.info('Started service, dropping tables');
-        return service.cleanup();
-    } else {
-        return true;
-    }
+  if (process.env.NODE_ENV === 'test') {
+    service.log.info('Started service, dropping tables');
+    return service.cleanup();
+  }
+
+  return true;
 }
 
 function prepareService() {
-    service.log.info('Started service, initiating tables');
-    return service.migrate();
+  service.log.info('Started service, initiating tables');
+  return service.migrate();
 }
 
 function serviceUp() {
-    service.log.info('Created tables');
+  service.log.info('Created tables');
 }
 
 function serviceCrashed(err) {
-    service.log.fatal('Failed to start service', err);
-    setImmediate(() => {
-        throw err;
-    });
+  service.log.fatal('Failed to start service', err);
+  setImmediate(() => {
+    throw err;
+  });
 }
 
-awaitCrate().bind(service).then(testPrepare).then(prepareService).then(serviceUp).then(service.connect).catch(serviceCrashed);
+awaitCrate()
+  .bind(service)
+  .then(testPrepare)
+  .then(prepareService)
+  .then(serviceUp)
+  .then(service.connect)
+  .catch(serviceCrashed);
--- bin/ping.js
+++ bin/ping.js
@@ -1,73 +1,75 @@
-//The MIT License (MIT)
+// The MIT License (MIT)
 //
-//Copyright (c) 2014 Adam Paszke
+// Copyright (c) 2014 Adam Paszke
 //
-//Permission is hereby granted, free of charge, to any person obtaining a copy
-//of this software and associated documentation files (the "Software"), to deal
-//in the Software without restriction, including without limitation the rights
-//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//copies of the Software, and to permit persons to whom the Software is
-//furnished to do so, subject to the following conditions:
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
-//The above copyright notice and this permission notice shall be included in all
-//copies or substantial portions of the Software.
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
 //
-//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-//SOFTWARE.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
-var net = require('net');
+/* eslint-disable no-use-before-define */
+const net = require('net');
 
-var ping = function(options, callback) {
-    var i = 0;
-    var results = false;
-    options.address = options.address || 'localhost';
-    options.port = options.port || 80;
-    options.attempts = options.attempts || 10;
-    options.timeout = options.timeout || 5000;
+const ping = (options, callback) => {
+  let i = 0;
+  let results = false;
+  options.address = options.address || 'localhost';
+  options.port = options.port || 80;
+  options.attempts = options.attempts || 10;
+  options.timeout = options.timeout || 5000;
 
-    var check = function(options, callback) {
-        if (i < options.attempts && !results) {
-            setTimeout(() => { connect(options, callback) }, options.timeout);
-        } else {
-            callback(undefined, results);
-        }
-    };
+  const connect = (opts, next) => {
+    const s = new net.Socket();
+    let timer;
 
-    var connect = function(options, callback) {
-        var s = new net.Socket();
-        var timer;
+    s.on('connect', () => {
+      clearTimeout(timer);
+      s.destroy();
+      results = true;
+      i++;
+      check(opts, next);
+    });
 
-        s.on('connect', function() {
-            clearTimeout(timer);
-            s.destroy();
-            results = true;
-            i++;
-            check(options, callback);
-        });
+    s.on('error', () => {
+      clearTimeout(timer);
+      s.destroy();
+      results = false;
+      i++;
+      check(opts, next);
+    });
 
-        s.on('error', function(e) {
-            clearTimeout(timer);
-            s.destroy();
-            results = false;
-            i++;
-            check(options, callback);
-        });
+    s.connect(opts.port, opts.address);
 
-        s.connect(options.port, options.address);
+    timer = setTimeout(() => {
+      results = false;
+      s.destroy();
+      i++;
+      check(opts, callback);
+    }, opts.timeout);
+  };
 
-        timer = setTimeout(function() {
-            results = false;
-            s.destroy();
-            i++;
-            check(options, callback);
-        }, options.timeout);
-    };
-    connect(options, callback);
+  const check = (opts, next) => {
+    if (i < options.attempts && !results) {
+      setTimeout(() => connect(opts, next), options.timeout);
+    } else {
+      next(null, results);
+    }
+  };
+
+  connect(options, callback);
 };
 
 module.exports.ping = ping;
