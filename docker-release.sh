68dda61df28c07ca6aa6a857eef1f409036b1e38
AVVS <v@makeomatic.ru>
2016-11-15 20:28:06 +0300

    chore: deploy script fix

--- docker-release.sh
+++ docker-release.sh
@@ -6,9 +6,10 @@ BUILD_ENV=${ENVS:-production}
 make ENVS="$BUILD_ENV" build push
 
 if [ x"$BRANCH_NAME" == x"master" ] && [ x"$SEMAPHORE" == x"true" ]; then
-  npm run doc
   git config user.email "semaphore@makeomatic.co"
   git config user.name "semaphore"
+  npm run doc
+  touch ./docs/.nojekyll
   git add -f ./docs
   git commit -m "docs($SEMAPHORE_BUILD_NUMBER): updated remote public documentation"
   git push origin `git subtree split --prefix docs master`:gh-pages --force
