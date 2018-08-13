6ae8f39a4594998b4f3d3e1b8c8b5f083736d1f6
Felipe Campos <felipegcampos@gmail.com>
2018-08-13 11:14:41 -0400

    fix: rfx single returning next 3 events from now (#37)

--- src/custom/rfx-post-single.js
+++ src/custom/rfx-post-single.js
@@ -1,10 +1,12 @@
 const RRule = require('rrule').RRule;
+const moment = require('moment');
 const _ = require('lodash');
 
 module.exports = function postSingle(data) {
   const rule = RRule.fromString(data.rrule);
+  const dates = rule.between(moment.utc().toDate(), rule.options.until);
 
   _.extend(data, {
-    start_time: _.take(rule.all(), 3),
+    start_time: _.take(dates, 3),
   });
 };
